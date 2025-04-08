92784f13b1735504866e20df3e5d062ecbf02c60
Darius Clark <dariusc93@users.noreply.github.com>
2025-04-08 09:01:01 -0400

    add Send

--- src/aio/mod.rs
+++ src/aio/mod.rs
@@ -20,5 +20,5 @@ pub(crate) const HEADER_NAME: &str = "SOAPAction";
 /// Trait to allow abstracting over `tokio` and `async-std`.
 pub trait Provider {
     /// Send an async request over the executor.
-    fn send_async(url: &str, action: &str, body: &str) -> impl Future<Output = Result<String, RequestError>>;
+    fn send_async(url: &str, action: &str, body: &str) -> impl Future<Output = Result<String, RequestError>> + Send;
 }
