c68ec8cfb3c97bcca2e77e37e55245624d703fbc
Darius Clark <dariusc93@users.noreply.github.com>
2025-07-24 17:34:50 -0400

    refactor: remove async-std (#16)

--- src/aio/mod.rs
+++ src/aio/mod.rs
@@ -5,11 +5,7 @@ mod gateway;
 #[cfg(feature = "aio_tokio")]
 pub mod tokio;
 
-#[cfg(feature = "aio_async_std")]
-pub mod async_std;
-
 use std::future::Future;
-
 use crate::RequestError;
 
 pub use self::gateway::Gateway;
@@ -17,7 +13,7 @@ pub use self::gateway::Gateway;
 pub(crate) const MAX_RESPONSE_SIZE: usize = 1500;
 pub(crate) const HEADER_NAME: &str = "SOAPAction";
 
-/// Trait to allow abstracting over `tokio` and `async-std`.
+/// Trait to allow abstracting over `tokio`.
 pub trait Provider {
     /// Send an async request over the executor.
     fn send_async(url: &str, action: &str, body: &str) -> impl Future<Output = Result<String, RequestError>> + Send;
